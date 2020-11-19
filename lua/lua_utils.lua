e14ae07019297abac2e9e99673083ae272523626
nialov <nikolasovaskainen@hotmail.com>
2020-11-19 18:33:20 +0200

    fixed lua

--- lua/lua_utils.lua
+++ lua/lua_utils.lua
@@ -1,21 +1,22 @@
 local treesit = require 'nvim-treesitter'
 
+function mytransform(line)
+    if line:match('->') then
+        line = line:gsub('.*>>>', '')
+    end 
+    matched = line:gsub('%s*[%[%(%{]*%s*$', '')
+                :gsub('def ', '')
+                :gsub('class ', '')
+                :match('([%a_%d]+)')
+                :gsub(' ', '')
+    -- line = line:gsub('def ', '')
+    -- line = line:gsub('class ', '')
+    -- final = line:match('([%a_%d]+)')
+    return matched
+end
+
 lua_utils = {}
 
-    function lua_utils.mytransform(line)
-        if line:match('->') then
-            line = line:gsub('.*>>>', '')
-        end 
-        matched = line:gsub('%s*[%[%(%{]*%s*$', '')
-                    :gsub('def ', '')
-                    :gsub('class ', '')
-                    :match('([%a_%d]+)')
-                    :gsub(' ', '')
-        -- line = line:gsub('def ', '')
-        -- line = line:gsub('class ', '')
-        -- final = line:match('([%a_%d]+)')
-        return matched
-    end
 
     function lua_utils.get_curr_parent()
         opts = {
