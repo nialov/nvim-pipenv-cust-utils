4ddd6da49e0537470b6271aba8d54809be46c415
nialov <nikolasovaskainen@hotmail.com>
2020-12-28 23:04:46 +0200

    proper namespacing

--- lua/lua_utils.lua
+++ lua/lua_utils.lua
@@ -1,32 +1,26 @@
 local treesit = require 'nvim-treesitter'
 
-function mytransform(line)
-    if line:match('->') then
-        line = line:gsub('.*>>>', '')
-    end 
-    matched = line:gsub('%s*[%[%(%{]*%s*$', '')
-                :gsub('def ', '')
-                :gsub('class ', '')
-                :match('([%a_%d]+)')
-                :gsub(' ', '')
+local M = {}
+
+function M.mytransform(line)
+    if line:match('->') then line = line:gsub('.*>>>', '') end
+    local matched = line:gsub('%s*[%[%(%{]*%s*$', ''):gsub('def ', ''):gsub(
+                        'class ', ''):match('([%a_%d]+)'):gsub(' ', '')
     -- line = line:gsub('def ', '')
     -- line = line:gsub('class ', '')
     -- final = line:match('([%a_%d]+)')
     return matched
 end
 
-lua_utils = {}
+function M.get_curr_parent()
+    local opts = {
+        indicator_size = 100,
+        type_patterns = {'class', 'function', 'method'},
+        transform_fn = M.mytransform,
+        separator = ' >>> '
+    }
+    local curr = treesit.statusline(opts)
+    return curr
+end
 
-
-    function lua_utils.get_curr_parent()
-        opts = {
-            indicator_size = 100,
-            type_patterns = {'class', 'function', 'method'},
-            transform_fn = mytransform,
-            separator = ' >>> '
-          }
-        curr = treesit.statusline(opts)
-        return curr
-    end
-
-return lua_utils
+return M
